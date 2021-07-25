4be6fd172bea83c251ec0c35479a4d12426f6188
Aliaksandr Zyl <zyl.alexander@gmail.com>
2021-07-25 16:16:27 +0200

    my first NN

--- book/cifar10_predict.py
+++ book/cifar10_predict.py
@@ -1,11 +1,13 @@
 from keras.datasets import cifar10
 import numpy as np
+import cv2
 
 from book.shallow_model import ShallowModel
 
 print("Loading model...")
 model = ShallowModel()
 model.load_model()
+label_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 print("Preparing Data...")
 (_, (testX, testY)) = cifar10.load_data()
@@ -19,7 +21,10 @@ randomY = np.take(testY, idx, axis=0)
 
 print("Predicting...")
 predictions = model.net.predict(randomX)
-print(testX)
 
-
-
+print("Show images")
+for image, p in zip(randomX, predictions.argmax(axis=1)):
+    resized = cv2.resize(image, (128, 128), interpolation=cv2.INTER_CUBIC)
+    cv2.putText(resized, label_names[p], (10, 30), cv2.FONT_ITALIC, 0.5, (0, 255, 0))
+    cv2.imshow("image", resized)
+    cv2.waitKey(0)
